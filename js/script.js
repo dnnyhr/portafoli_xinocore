60854d4336b847da5bc9c6db257557ef3c380255
dnnyhr <entitydh@gmail.com>
2025-01-20 23:37:00 -0600

    Increase timeout duration from 5000ms to 8000ms in script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ setTimeout(() => {
         ease: "power2.inOut", 
         overwrite: "auto" 
     });
-}, 5000); 
+}, 8000); 
 gsap.utils.toArray('.project-card').forEach((card, i) => {
     gsap.to(card, {
         scrollTrigger: {
