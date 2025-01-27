a64fa8690b9ee4f8ba02c0869274de01424a835e
dnnyhr <entitydh@gmail.com>
2025-01-27 14:55:25 -0600

    bugfix3

--- js/script.js
+++ js/script.js
@@ -13,6 +13,10 @@ function closeMenu() {
     const menu = document.getElementById('navMenu');
     menu.classList.remove('active');
 }
+// Forzar ajuste del tamaño en dispositivos móviles para evitar desbordamiento
+window.addEventListener('resize', () => {
+    document.body.style.overflowX = 'hidden'; // Asegura que no haya desbordamiento al cambiar el tamaño
+});
 
 // Cerrar menú al hacer scroll
 window.addEventListener('scroll', closeMenu);
