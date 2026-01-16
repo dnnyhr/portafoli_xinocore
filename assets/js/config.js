778839b7440c72725ebe2fc0ca54b956e959317a
Danny-Herrod <Sisimiquito@gmail.com>
2026-01-15 23:25:16 -0600

    fix: Set development mode to false for production environment

--- assets/js/config.js
+++ assets/js/config.js
@@ -10,7 +10,7 @@ const XinocoreConfig = {
      * true = Mostrar logs en consola
      * false = Ocultar todos los logs (modo producción)
      */
-    development: true,
+    development: false,
 
     /**
      * Información de contacto
